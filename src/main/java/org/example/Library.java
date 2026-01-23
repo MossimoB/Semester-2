c04f401e578497773428f7155d9d197177ca2de3
2585451 <2585451@edu.vaniercollege.qc.ca>
2026-01-23 10:33:15 -0500

    added more stuff

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -2,6 +2,7 @@ package org.example;
 
 import lombok.AllArgsConstructor;
 
+import javax.xml.namespace.QName;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,5 +12,46 @@ public class Library {
 
     public Library() {
         this.books = new ArrayList<>();
+    }
+
+    /**
+     * add a new book to the books of the library
+     * @param book the new book to be added
+     */
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    /**
+     * searches books that contains a keyword
+     * @param keyword the keyword to be searched
+     * @return the list of books which contain the book
+     */
+    public List<Book> searchBooks(String keyword) {
+        List<Book> result = new ArrayList<>();
+
+        // here is where you would go through all the books
+        // that are stored in the library
+
+        // searches for the book
+        for (Book book : books) {
+            // searches using the title
+            String title = book.getTitle().toLowerCase();  // changes title to lowercase
+            if (title.contains(keyword)) {
+                result.add(book); // if the title has the word, add the book
+            }
+
+            // searches using the author
+            Author[] authors = book.getAuthors();
+            for (Author author : authors) {
+                String findAuthor = author.getName().toLowerCase(); // changes author to lowercase
+                if (name.contains(keyword)) {
+                    result.add(book); // if the author had the keyword, add the book
+                }
+            }
+
+        }
+
+        return result;
     }
 }
