5b80e20a00ed59db55ddf99b065041b9c472530d
2585451 <2585451@edu.vaniercollege.qc.ca>
2026-01-23 09:27:27 -0500

    added more stuff

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,13 @@ public class Main {
         // array is fixed
         // list is dynamic
 
-        //init
+        // init
         String str = "hello";
-        double[] nums1 = {5, 6, 7}; // array with specific values
+        double[] nums1 = {5, 6, 7};               // array with specific values
         double[] nums2 = new double[5];
-        List<Double> nums3 = new ArrayList<>(); // inheritance
+        List<Double> nums3 = new ArrayList<>();   // inheritance
 
-        //get element
+        // get element
         char c = str.charAt(0);
         double num1 = nums1[0];
         double num3 = nums3.get(0);
@@ -29,6 +29,36 @@ public class Main {
         int arratLen = nums1.length;
         int listLen = nums3.size();
 
-        
+        // add/remove an element (list)
+        nums3.add(9.0);                           // append at the end
+        nums3.add(0, 9.0);          // add at the beginning
+        nums3.remove(0);                   // removes based on the index
+        nums3.remove(9.0);                   // removes based on the value
+
+        // loops
+        for (char chara : str.toCharArray()) {
+            System.out.println(chara);
+        }
+
+        for (double num : nums1) {              // array
+            System.out.println(num);
+        }
+
+        for (double num : nums3) {             // list
+            System.out.println(num);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i]++;
+        }
+
+        // write a for loop to go through each element in a list and add 1 to them
+        for (int i = 0; i < nums1.length; i++) {
+            nums3.set(i, nums3.get(i) + 1);
+        // this loop goes through each index in the list nums3, gets its current value, then adds 1
+        }
+
+        // compare (same or not)
+
     }
 }
