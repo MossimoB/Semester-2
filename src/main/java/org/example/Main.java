7c50d40408623d5d8b2eb8cfd7d4de0ae823e437
2585451 <2585451@edu.vaniercollege.qc.ca>
2026-01-23 09:37:04 -0500

    added more stuff

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -10,9 +11,9 @@ public class Main {
 
         // init
         String str = "hello";
-        double[] nums1 = {5, 6, 7};               // array with specific values
+        double[] nums1 = {5, 6, 7};                                          // array with specific values
         double[] nums2 = new double[5];
-        List<Double> nums3 = new ArrayList<>();   // inheritance
+        List<Double> nums3 = new ArrayList<>();                              // inheritance
 
         // get element
         char c = str.charAt(0);
@@ -30,21 +31,21 @@ public class Main {
         int listLen = nums3.size();
 
         // add/remove an element (list)
-        nums3.add(9.0);                           // append at the end
-        nums3.add(0, 9.0);          // add at the beginning
-        nums3.remove(0);                   // removes based on the index
-        nums3.remove(9.0);                   // removes based on the value
+        nums3.add(9.0);                                                    // append at the end
+        nums3.add(0, 9.0);                                   // add at the beginning
+        nums3.remove(0);                                            // removes based on the index
+        nums3.remove(9.0);                                            // removes based on the value
 
         // loops
         for (char chara : str.toCharArray()) {
             System.out.println(chara);
         }
 
-        for (double num : nums1) {              // array
+        for (double num : nums1) {                                       // array
             System.out.println(num);
         }
 
-        for (double num : nums3) {             // list
+        for (double num : nums3) {                                       // list
             System.out.println(num);
         }
 
@@ -59,6 +60,15 @@ public class Main {
         }
 
         // compare (same or not)
+        boolean strEqual = str.equals(str);                             // string
+        boolean arrayEqual = Arrays.equals(nums1, nums2);               // array
+        boolean listEquals = nums3.equals(nums3);                       // list
+
+        // check if empty
+        boolean strEmpty = str.isEmpty();                              // string
+        boolean arrayEmpty = nums1.length == 0;                        // array
+        boolean listEmpty = nums3.isEmpty();                           // list
+
 
     }
 }
