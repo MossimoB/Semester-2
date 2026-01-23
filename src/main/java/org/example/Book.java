65b618af06aa302bd8093737cdd5c0cd4eaab649
2585451 <2585451@edu.vaniercollege.qc.ca>
2026-01-23 10:15:58 -0500

    added more stuff

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class Book {
+    private String id;
+    private String title;
+    private Author[] authors; // author array
+
+    private static int nextId = 1;
+
+    public Book(String name, Author[] authors) {
+        this.id = String.format("%04d", nextId++);
+        this.title = title;
+        this.authors = authors;
+    }
 }
--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
 public class Library {
+    private List<Book> books; // a list of books, NOT array
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
 }
