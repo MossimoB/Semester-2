79f11368bb644f9572967bb6c1f92ca9a87fc37a
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-02-03 09:35:42 -0500

    Update Main.java

--- Inheritance/src/main/java/org/mossimo/Main.java
+++ Inheritance/src/main/java/org/mossimo/Main.java
@@ -1,12 +1,23 @@
 package org.mossimo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Pet pet = new Cat("Snow", 3, 2);
-        
         Object object1 = new Cat("Snow", 3, 2);
         Object object2 = new Dog("Snow", 3, false);
 
-        System.out.println(pet.equals(object1)); // polymorphism
+        List<Pet> pets = new ArrayList<>();
+
+        // upper casting
+        pets.add(new Cat("Snow", 3, 2));
+        pets.add(new Dog("Snow", 3, false));
+        pets.add(new Pet("Snow", 3)); // this line won't work if Pet class is abstract
+                                                 // because the class cannot contain objects if abstract
+
+        for (Pet pet : pets) {
+            pet.makeSound();    // polymorphism
+        }
     }
 }
