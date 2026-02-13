85bcf05a9df28123964246c9777007dd58e14dee
MossimoB <2585451@edu.vaniercollege.qc.ca>
2026-02-13 10:22:04 -0500

    Update Main.java

--- Recursion/src/main/java/org/mossimo/Main.java
+++ Recursion/src/main/java/org/mossimo/Main.java
@@ -2,7 +2,17 @@ package org.mossimo;
 
 public class Main {
     public static void main(String[] args) {
+        // FIBONACCI SEQUENCE
+        int num = 10;
+        System.out.println(fibonacci1(num));
+        System.out.println(fibonacciRecursion(num));
 
+        // TOWER OF HANOI
+        int n = 10; // number of disks
+        hanoi(n, 'A', 'C', 'B');
+        // Calculate and print the total number of moves
+        int totalMoves = (int) Math.pow(2, n) - 1;
+        System.out.println("Total moves: " + totalMoves);
     }
 
     /**
@@ -49,22 +59,118 @@ public class Main {
         return reverseRecursion(str.substring(1)) + str.charAt(0);
     }
 
+    public static boolean isPalindrome(String str) {
+        if (str.length() <= 1) {
+            return true;
+        }
 
-    // THIS IS INCOMPLETE, LOOK AT SOURCE CODE ON OMNIVOX
+        // Compare first and last characters
+        // If they match, recursively check the substring
+        // that excludes the first and last characters
+        // The string is a palindrome only if BOTH conditions are true
+        int len = str.length();
+        return str.charAt(0) == str.charAt(len - 1) &&
+                isPalindrome(str.substring(1, len -1));
+    }
 
-//    public static boolean isPalindrome(String str) {
-//        for (int i = 0; i <= str.length() / 2; i++) {
-//
-//        }
-//
-//        return false;
-//    }
-//
-//    public static boolean isPalindromeRecursion(String str) {
-//        if (str.length() <= 1) {
-//            return true;
-//        }
-//
-//        return isPalindromeRecursion();
-//    }
+    /**
+     * f(0) = 0
+     * f(1) = 1
+     * f(2) = f(0) + f(1) = 1
+     * f(3) = f(1) + f(2) = 2
+     * f(n) = f(n-1) + f(n-2)
+     * @param num
+     * @return
+     */
+    public static int fibonacciRecursion(int num) {
+        if (num <= 1) {
+            return num;
+        }
+
+        return fibonacciRecursion(num - 1) + fibonacciRecursion(num - 2);
+    }
+
+    public static int fibonacci1(int num) {
+        // Base cases
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+
+        // Recursive case
+        return fibonacci1(num - 1) + fibonacci1(num - 2);
+    }
+
+    public static int fibonacci2(int num) {
+        // Base cases
+        if (num == 0)  {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+
+        // Create array to store Fibonacci values up to num
+        int[] fib = new int[num + 1];
+
+        // Base cases
+        fib[0] = 0;
+        fib[1] = 1;
+
+        // Filling the array
+        for (int i = 2; i <= num; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib[num];
+    }
+
+        // This method solves the Tower of Hanoi problem using recursion
+        // It moves n disks from the source rod to the destination rod using an auxiliary rod
+        // The idea: move n-1 disks to the auxiliary rod, move the largest disk to the destination,
+        // then move the n-1 disks from auxiliary to destination. The base case is n == 1
+        public static void hanoi(int n, char from, char to, char aux) {
+            // base case
+            if (n == 0) {
+                System.out.println("Move disk 1 from " + from + " to " + to);
+                return;
+            }
+
+            // Move n-1 disks from source to auxiliary (helper rod)
+            // Move the top (n-1) disks from 'from' (start) to 'aux' (helper), using 'to' (destination) as temporary storage
+            hanoi(n - 1, from, aux, to);
+
+            // Move the largest disk
+            System.out.println("Move disk " + n + " from " + from + " to " + to);
+
+            // Move n-1 disks from auxiliary to destination
+            // Move the top (n-1) disks from 'aux' (helper) to 'to' (destination), using 'from' (source) as temporary storage
+            hanoi(n - 1, aux, to, from);
+        }
+
+        // only consider positive exponents
+        // n^0 = 1
+        // 2^5 = 2 2 2 2 2
+        // 3^5 = 3 3 3 3 3
+        public static int pow(int base, int exp) {
+            // base case
+            if (exp == 0) {
+                return 1;
+            }
+            // recursive step
+            return base * pow(base, exp - 1);
+        }
+
+        // only consider positive num
+        // 123 = 1 + 2 + 3 = 6
+        public static int sumDigit(int num) {
+            // base case
+            if (num == 0) {
+                return 0;
+            }
+            // recursive step
+            return (num % 10) + sumDigit(num / 10);
+        }
 }
